e1c79e4479b03b07aba54706e11775a16c2f42ca
Jigeesha Jain <90517477+JigeeshaJain@users.noreply.github.com>
2022-02-17 07:53:56 -0500

    Time: 247 ms (7.41%), Space: 14.2 MB (86.30%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -3,7 +3,7 @@ class Solution:
         if needle=="":
             return 0
         
-        for i in range(len(haystack)+1-len(needle)):
+        for i in range(len(haystack)):
             if haystack[i:i+len(needle)]==needle:
                 return i
         return -1
