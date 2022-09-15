cb78a5da8fc1f3e1fccba93cce98da3cf14749e4
Jigeesha Jain <90517477+JigeeshaJain@users.noreply.github.com>
2022-09-15 14:40:56 -0400

    Time: 2828 ms (17.40%), Space: 14 MB (31.12%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -2,6 +2,7 @@ from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         c1=Counter(s1) #When you use Counter on this list , it will count how many times x , y and z is present. 
+        print(c1)
         l1=len(s1)
         l2=len(s2)
         for i in range(0,l2-l1+1):
