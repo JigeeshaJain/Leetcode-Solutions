2e9c997cab93b9eb40065cb2521763019de6c4af
Jigeesha Jain <90517477+JigeeshaJain@users.noreply.github.com>
2022-10-06 12:10:08 -0400

    Time: 29 ms (96.63%), Space: 14 MB (20.04%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,14 +1,34 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         
-        def helper(x,n):
-            if x==0: return 0
-            if n==0: return 1 
+        #Deal with negative power:
+        if n < 0: 
+            x = 1/x
+            n = abs(n)
             
-            res= helper(x,n//2)
-            res=res*res
-            return x*res if n%2 else res
-        res=helper(x, abs(n))
-        return res if n>0 else 1/res
+        #Iterate:
+        res = 1
+        while n:
+            if n % 2:
+                res = res*x
+            x = x*x 
+            n = n//2
+            
+        return res
+        
+        
+        
+        
+        
+        
+#         def helper(x,n):
+#             if x==0: return 0
+#             if n==0: return 1 
+            
+#             res= helper(x,n//2)
+#             res=res*res
+#             return x*res if n%2 else res
+#         res=helper(x, abs(n))
+#         return res if n>0 else 1/res
            
         
