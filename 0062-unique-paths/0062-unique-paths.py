1775000521849e6be080695398686c506854fb13
Jigeesha Jain <90517477+JigeeshaJain@users.noreply.github.com>
2023-03-27 14:24:48 -0400

    Time: 35 ms (47.50%), Space: 13.8 MB (97.30%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -2,10 +2,10 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         #dynamic programming
         
-        row=[1]*n
+        row=[1]*n #this is going to be the bottom row 
         
-        for i in range(m-1):
-            newRow=[1]*n
+        for i in range(m-1): #m-1 is taken as last row is all ones 
+            newRow=[1]*n #initially it will have all 1s as its value 
             for j in range(n-2,-1,-1):
                 newRow[j]= newRow[ j + 1] + row[ j ]
             row=newRow
